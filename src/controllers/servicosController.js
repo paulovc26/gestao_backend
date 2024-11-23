cabab9abd6be95b6137190a9b50b591d9fcba6ed
paulovc26 <paulovcsiqueira@gmail.com>
2024-11-23 14:00:01 -0300

    update servicosController

--- src/controllers/servicosController.js
+++ src/controllers/servicosController.js
@@ -5,10 +5,10 @@ const getAll = async (req, res) => {
     const servicos = await servicosModel.getAllServicos();
     return res.status(200).json({ servicos });
   } catch (error) {
-    console.error("Erro ao obter servicos:", error);
+    console.error("Erro ao obter serviços:", error);
     return res
       .status(500)
-      .json({ message: "Erro ao obter servicos", error: error.message });
+      .json({ message: "Erro ao obter serviços", error: error.message });
   }
 };
 
@@ -17,10 +17,10 @@ const createServico = async (req, res) => {
     const createdServico = await servicosModel.createServico(req.body);
     return res.status(201).json(createdServico);
   } catch (error) {
-    console.error("Erro ao criar servico:", error);
+    console.error("Erro ao criar serviço:", error);
     return res
       .status(500)
-      .json({ message: "Erro ao criar servico", error: error.message });
+      .json({ message: "Erro ao criar serviço", error: error.message });
   }
 };
 
@@ -31,10 +31,10 @@ const deleteServico = async (req, res) => {
     await servicosModel.deleteServico(id);
     return res.status(204).json({});
   } catch (error) {
-    console.error("Erro ao deletar servico:", error);
+    console.error("Erro ao deletar serviço:", error);
     return res
       .status(500)
-      .json({ message: "Erro ao deletar servico", error: error.message });
+      .json({ message: "Erro ao deletar serviço", error: error.message });
   }
 };
 
@@ -44,7 +44,7 @@ const updateServico = async (req, res) => {
 
   if (!servico || Object.keys(servico).length === 0) {
     return res.status(400).json({
-      message: "Dados do servico não fornecidos",
+      message: "Dados do serviço não fornecidos",
     });
   }
 
@@ -52,16 +52,16 @@ const updateServico = async (req, res) => {
     const updatedServico = await servicosModel.updateServico(id, servico);
 
     if (updatedServico.affectedRows === 0) {
-      return res.status(404).json({ msg: "Dados do servico não encontrado" });
+      return res.status(404).json({ msg: "Dados do serviço não encontrado" });
     }
 
     return res
       .status(200)
-      .json({ msg: "Dados do servico atualizados com sucesso!" });
+      .json({ msg: "Dados do serviço atualizados com sucesso!" });
   } catch (error) {
-    console.error("Erro ao atualizar servico:", error);
+    console.error("Erro ao atualizar serviço:", error);
     return res.status(500).json({
-      msg: "erro ao atualizar o servico",
+      msg: "erro ao atualizar o serviço",
       error: error.message,
     });
   }
