a83c23634d97c8bb749c2ad1e2925d0bea8206ba
paulovc26 <paulovcsiqueira@gmail.com>
2024-11-28 10:55:05 -0300

    fix middleware

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+const authMiddleware = require("../middlewares/authMiddleware");
+
 const usersController = require("../controllers/usersController");
 
 /**
