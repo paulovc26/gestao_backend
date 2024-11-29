3f2b9d3db69163df1dbb4f67e91fa22b4bfd0e8f
paulovc26 <paulovcsiqueira@gmail.com>
2024-11-28 21:55:06 -0300

    update cors

--- src/server.js
+++ src/server.js
@@ -6,7 +6,11 @@ const app = require("./app");
 require("dotenv").config();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    methods: ["GET", "POST", "PUT", "DELETE"], // Garanta que os métodos necessários estão permitidos
+  })
+);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
